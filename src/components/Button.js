181930467c171e3e9d0615a724195f3421cfb35d
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-21 13:58:32 -0500

    Buttons are built in isolation

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,17 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  const { children, confirm, danger, onClick, disabled } = props;
+
+  let buttonClass = "button";
+
+  if (confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (danger) {
+    buttonClass += " button--danger";
+  }
+
+  return <button onClick={onClick} disabled={disabled} className={buttonClass}>{children}</button>;
 }
