b4bbc9adb2adf76ecd81177191fe89e668718649
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-21 15:12:34 -0500

    Implement and test DayList component, also used map method

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DayListItem from "./DayListItem";
 
 /**
  * PROPS NEEDED
@@ -9,5 +10,22 @@ import React from "react";
  */
 
 export default function DayList(props) {
-  const { days, day, setDay } = props;
+
+  const parsedDays = props.days.map((day) => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+
+  return (
+    <ul>
+      {parsedDays}
+    </ul>
+  );
 }
