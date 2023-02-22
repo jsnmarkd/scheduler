9c79e5cf6781b204b134556d7afa63f1c01c382b
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-22 13:59:04 -0500

    Implement Interviewer List with SCSS and stories

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,39 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+/**
+ * PROPS NEEDED
+ *
+ * interviewers: array - an array of objects as seen above
+ * setInterviewer: function - a function that accepts an interviewer id. This function will simply be passed down to the <InterviewerListItem>
+ * interviewer: number - a number that represents the id of the currently selected interviewer
+ * @param {*} props
+ * @returns
+ */
 
 export default function InterviewerList(props) {
+  const parsedInterviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    )
+  });
+
   return (
     <ul>
-      <InterviewerListItem />
+      <section className="interviewers">
+        <h4 className="interviewers__header text--light">Interviewer</h4>
+        <ul className="interviewers__list">
+          {parsedInterviewers}
+        </ul>
+      </section>
     </ul>
   );
 }
