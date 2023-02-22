ab5c760213231732ed6146e2369bff2730e337a5
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-22 18:42:19 -0500

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/index.scss";
 
 import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      {props.interview ? (
+        <>
+          <Header time={props.time} />
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer.name}
+          />
+        </>
+      ) : (
+        <>
+          <Header time={props.time} />
+          <Empty />
+        </>
+      )}
+    </article>
+  );
 }
