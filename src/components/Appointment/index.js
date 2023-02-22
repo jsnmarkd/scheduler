f9ceb716c748f6c59ca38b821776f697e06cec40
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-22 15:54:59 -0500

    Implement Header and Empty of the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "components/Appointment/index.scss";
 
+import Header from "./Header";
+
 export default function Appointment(props) {
   return <article className="appointment"></article>;
 }
