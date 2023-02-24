67587bb24c4ed15d20ef261a2389939879c41994
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-23 22:20:19 -0500

    Retrieved Appointments-api

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,62 +5,32 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
+    appointments: {},
   });
  
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   useEffect(() => {
-    const url = `http://localhost:8001/api/days`;
-    axios.get(url).then((res) => {
-      console.log(res.data);
-      const setDays = res.data;
-      setState({...state, days: setDays});
+    const urlDays = `http://localhost:8001/api/days`;
+    const urlAppointments = `http://localhost:8001/api/appointments`;
+    // const urlInterviewers = `http://localhost:8001/api/interviewers`;
+    Promise.all([
+      axios.get(urlDays),
+      axios.get(urlAppointments),
+    ]).then((all) => {
+      const setDays = all[0].data;
+      const setAppointments = all[1].data;
+      setState((prev) => ({ ...prev, days: setDays, appointments: setAppointments }));
     });
   }, []);
 
-  const appointmentComponents = Object.values(appointments).map(
+  const appointmentComponents = dailyAppointments.map(
     (appointment) => {
       return <Appointment key={appointment.id} {...appointment} />;
     }
